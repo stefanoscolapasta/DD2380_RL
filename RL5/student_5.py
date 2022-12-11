68fbbd6aa2c8ce424390f0aece07a4d14cb1f941
StefanoScolari <stefano.scolari@studio.unibo.it>
2022-12-11 13:57:19 +0100

    Initial commit

---
+++ RL5/student_5.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Q learning learning rate
+alpha = .4
+
+# Q learning discount rate
+gamma = .2
+
+# Epsilon initial
+epsilon_initial = 1
+
+# Epsilon final
+epsilon_final = .2
+
+# Annealing timesteps
+annealing_timesteps = 100
+
+# threshold
+threshold = 1e-6
